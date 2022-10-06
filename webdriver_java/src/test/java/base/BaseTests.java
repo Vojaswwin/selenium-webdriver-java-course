3092153da0d4a91a2523cc1ba788b2b6e2d8d7a2
Vojaswwin <vojaswwin.ap@thoughtworks.com>
2022-10-06 12:37:40 +0530

    Modify. Base test to include linux extension based on OS env variable

--- webdriver_java/src/test/java/base/BaseTests.java
+++ webdriver_java/src/test/java/base/BaseTests.java
@@ -1,12 +1,16 @@
 package base;
 
 import com.google.common.io.Files;
-import org.openqa.selenium.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.ITestResult;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 import utils.CookieManager;
 import utils.EventReporter;
@@ -17,53 +21,56 @@ import java.io.IOException;
 
 public class BaseTests {
 
-    private EventFiringWebDriver driver;
     protected HomePage homePage;
+    private EventFiringWebDriver driver;
 
     @BeforeClass
-    public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+    public void setUp() {
+        var driverExtention = "";
+        if (System.getenv("RUNNER_OS") != null) {
+            driverExtention = "-linux";
+        }
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver" + driverExtention);
         driver = new EventFiringWebDriver(new ChromeDriver(getChromeOptions()));
         driver.register(new EventReporter());
     }
 
     @BeforeMethod
-    public void goHome(){
+    public void goHome() {
         driver.get("https://the-internet.herokuapp.com/");
         homePage = new HomePage(driver);
     }
 
     @AfterClass
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
     }
 
     @AfterMethod
-    public void recordFailure(ITestResult result){
-        if(ITestResult.FAILURE == result.getStatus())
-        {
-            var camera = (TakesScreenshot)driver;
+    public void recordFailure(ITestResult result) {
+        if (ITestResult.FAILURE == result.getStatus()) {
+            var camera = (TakesScreenshot) driver;
             File screenshot = camera.getScreenshotAs(OutputType.FILE);
-            try{
+            try {
                 Files.move(screenshot, new File("resources/screenshots/" + result.getName() + ".png"));
-            }catch(IOException e){
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    public WindowManager getWindowManager(){
+    public WindowManager getWindowManager() {
         return new WindowManager(driver);
     }
 
-    private ChromeOptions getChromeOptions(){
+    private ChromeOptions getChromeOptions() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("disable-infobars");
         //options.setHeadless(true);
         return options;
     }
 
-    public CookieManager getCookieManager(){
+    public CookieManager getCookieManager() {
         return new CookieManager(driver);
     }
 }
